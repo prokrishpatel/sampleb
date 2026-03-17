044fc7282f0c34777f4ccd3ab3db0973c3407fd7
prokrishpatel <pro.krishpatel@gmail.com>
2026-03-17 21:40:00 +0530

    bufix

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,9 +2,9 @@ import { v2 as cloudinary } from 'cloudinary';
 import fs from 'fs';
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    api_secret: process.env.COULDINARY_API_SECRET
 })
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -16,6 +16,7 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto",
         });
         console.log("File uploaded on cloudinary", response.url);
+        fs.unlinkSync(localFilePath);
         return response;
     }
     catch (error) {
